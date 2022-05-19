5a660b64bd9eec8d0f7536bc4dae7d3f62423f53
DayanePA <dayane.almeida2007@gmail.com>
2022-05-19 22:44:14 +0100

    Conexão com banco de dados, com as dependencia necessarias as configuraçoes do application.properties,anotacoes do banco de dados como: Entity, Column,Id.

--- src/main/java/com/example/cadastro/domain/Pessoa.java
+++ src/main/java/com/example/cadastro/domain/Pessoa.java
@@ -1,24 +1,32 @@
 package com.example.cadastro.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
+@Entity(name = "pessoa")
 public class Pessoa {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
 
-  private int id;
-
+  @Column(length = 50)
   @NotBlank(message = "{name.not.blank}")
   private String name;
 
+  @Column(name = "last_name", length = 50)
   @NotBlank(message = "{lastName.not.blank}")
   private String lastName;
 
+  @Column(name = "birth_date", length = 10)
   private String birthDate;
-
-
 
 }
